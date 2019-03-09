6e4645247b52662f80270b7ee403f8fa889bb76e
wk-j <somnuk.wk@gmail.com>
2019-03-09 15:06:48 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ## Run Code Server
+
+[![NuGet](https://img.shields.io/nuget/v/wk.RunCode.svg)](https://www.nuget.org/packages/wk.RunCode)
+
+## Installation
+
+```bash
+dotnet tool install -g wk.RunCode
+```
+
+## Usage
+
+```bash
+wk-run-code x/y/z/Project
+open http://localhost:8888
+```
--- src/RunCode/Program.fs
+++ src/RunCode/Program.fs
@@ -9,8 +9,10 @@ let main argv =
         | _ -> DirectoryInfo(".").FullName
 
     let command =
-        """docker run --name code-server -p 8888:8443 -v "{path}:/root/project" codercom/code-server code-server --allow-http --no-auth"""
+        """docker run -d --name code-server -p 8888:8443 -v "{path}:/root/project" codercom/code-server code-server --allow-http --no-auth"""
             .Replace("{path}", path)
 
+    PS("docker stop code-server")
+    PS("docker rm code-server")
     PS(command)
     0
